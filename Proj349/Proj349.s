2fb99f996c7afb74db53ad36d0df5b5e6b475d5e
koltonmyers01 <151096652+koltonmyers01@users.noreply.github.com>
2023-11-28 11:51:50 -0600

    Update Proj349.s
    ill check on later, but here is a rough idea so far

--- Proj349/Proj349.s
+++ Proj349/Proj349.s
@@ -1,124 +1,178 @@
 		area proj349, code, readonly
+RS      equ 0x20    ; RS connects to P3.5
+RW      equ 0x40    ; RW connects to P3.6
+EN      equ 0x80    ; EN connects to P3.7
+export __main
 
-__main		proc	
+; Define obstacle positions
+OBSTACLE_TOP_RIGHT      EQU 0
+OBSTACLE_BOTTOM_RIGHT   EQU 1
+OBSTACLE_NONE           EQU 2
 
-			BL LCDInit
-			
-			MOV R3, #'S'			; Character 'S'	
-			BL LCDData				; Send character in R3 to LCD
-			
-			; ADD INSTRUCTIONS TO SEND REMAINING DATA TO THE LCD
-			MOV R3, #'U'			; Character 'U'	
-			BL LCDData				; Send character in R3 to LCD
-			
-			MOV R3, #'C'			; Character 'C'	
-			BL LCDData				; Send character in R3 to LCD
-			
-			MOV R3, #'C'			; Character 'C'	
-			BL LCDData				; Send character in R3 to LCD
+; Global variables
+obstaclePosition        DCD 0
+playerPosition          DCD 0
 
-			MOV R3, #'E'			; Character 'E'	
-			BL LCDData				; Send character in R3 to LCD
 
-			MOV R3, #'S'			; Character 'S'	
-			BL LCDData				; Send character in R3 to LCD
+__main  proc
 
-			MOV R3, #'S'			; Character 'S'	
-			BL LCDData				; Send character in R3 to LCD
-			
-			MOV R2, #0xC7			; New Line
-			BL LCDCommand
+    ; Initialize LCD
+    BL LCDInit
 
-			MOV R3, #'F'			; Character 'F'	
-			BL LCDData				; Send character in R3 to LCD
+    ; Display '0' in the top-left corner
+    MOV R3, #'0'            ; Character '0'
+    MOV R0, #0              ; Row 0
+    MOV R1, #0              ; Column 0
+    BL LCDSetCursorPosition ; Set cursor position
+    BL LCDData              ; Send character in R3 to LCD
 
-			MOV R3, #'A'			; Character 'A'	
-			BL LCDData				; Send character in R3 to LCD
+    ; Set initial obstacle position
+    MOV R0, #1              ; Set default row to 1 (top right)
+    BL RandomPosition       ; Get random position (0, 1, or 2)
+    STR R0, [obstaclePosition]
 
-			MOV R3, #'I'			; Character 'I'	
-			BL LCDData				; Send character in R3 to LCD
+    ; Set initial player position
+    MOV R0, #0              ; Set row to 0 (top left)
+    MOV R1, #0              ; Set column to 0
+    STR R1, [playerPosition]
 
-			MOV R3, #'L'			; Character 'L'	
-			BL LCDData				; Send character in R3 to LCD
+    ; Game loop
+gameLoop
+    ; Display obstacles ('X') and player based on their current positions
+    MOV R3, #'X'            ; Character 'X'
+    LDR R0, [obstaclePosition]
+    BL DisplayObstacle      ; Display obstacle at the current position
 
-			MOV R3, #'U'			; Character 'U'	
-			BL LCDData				; Send character in R3 to LCD
+    MOV R3, #'0'            ; Character 'P' for player
+    LDR R0, [playerPosition]
+    BL DisplayPlayer        ; Display player at the current position
 
-			MOV R3, #'R'			; Character 'R'	
-			BL LCDData				; Send character in R3 to LCD
+    ; Move obstacle from right to left
+    MOV R0, #0              ; Row (not used in this case)
+    MOV R1, #1              ; Move left by 1 column
+    BL MoveObstacle
 
-			MOV R3, #'E'			; Character 'E'	
-			BL LCDData				; Send character in R3 to LCD
+    ; Move player based on input (you need to implement this part)
+    ; For simplicity, let's assume the player moves one column to the right each time
+    MOV R0, #0              ; Row 0 (top)
+    MOV R1, #1              ; Move right by 1 column
+    BL MovePlayer
 
-stay		B stay					; Remain here after completion
-			endp
-				
-				
-LCDInit		function
-					
-			LDR R0, =0x40004C20		; P3: control pins
-			LDR R1, =0x40004C21		; P4: data or commands 		
-			MOV R2, #0xE0			; 1110 0000 
-			STRB R2, [R0, #0x04]	; outputs pins for EN: 3.7, RW: 3.6, RS: 3.5
-			MOV R2, #0xFF
-			STRB R2, [R1, #0x04]	; All of Port 4 as output pins to LCD
-			
-			PUSH {LR}		
-			MOV R2, #0x38			; 2 lines, 7x5 characters, 8-bit mode		 
-			BL LCDCommand			; Send command in R2 to LCD
+    ; Check for collision
+    BL CheckCollision
 
-			MOV R2, #0x0E			; TURN ON THE DISPLAY AND THE CURSOR
-			BL LCDCommand
-			
-			MOV R2, #0x01			; CLEAR DISPLAY
-			BL LCDCommand
-			
-			MOV R2, #0x06			; MOVE CURSOR RIGHT
-			BL LCDCommand
-				
-			POP {LR}			
-			BX LR
-			endp
-				
-LCDCommand	function				; R2 brings in the command byte
-			STRB R2, [R1, #0x02]
-			MOV R2, #0x00			; RS = 0, command register selected, RW = 0, write to LCD
-			ORR R2, EN
-			STRB R2, [R0, #0x02]	; EN = 1
-			PUSH {LR}
-			BL delay
-			
-			MOV R2, #0x00
-			STRB R2, [R0, #0x02]	; EN = 0 and RS = RW = 0	
-			POP {LR}
-			BX LR
-			endp				
-				
-LCDData		function				; R3 brings in the character byte
-			; COMPLETE THIS FUNCTION, REFER TO LCDCommand and TABLE 3 on HANDOUT
-			STRB R3, [R1, #0x02]
-			MOV R2, #0x20			; RS = 1, command register selected, RW = 0, write to LCD
-			ORR R2, EN
-			STRB R2, [R0, #0x02]	; EN = 1
-			PUSH {LR}
-			BL delay
-			
-			MOV R2, #0x20
-			STRB R2, [R0, #0x02]	; EN = 0, RW = 0, and RS = 1	
-			POP {LR}
-			BX LR
-			endp
-		
-delay		function
-			MOV R5, #50
-loop1		MOV R4, #0xFF
-loop2		SUBS R4, #1
-			BNE loop2
-			SUBS R5, #1
-			BNE loop1
-			BX LR
-			endp
-			
-			end		
+    ; Delay to control the speed of the movement
+    BL delay
 
-		END
+    ; Clear previous obstacle and player positions
+    LDR R0, [obstaclePosition]
+    MOV R3, #' '            ; Clear the obstacle
+    BL DisplayObstacle      ; Display space at the previous obstacle position
+
+    LDR R0, [playerPosition]
+    MOV R3, #' '            ; Clear the player
+    BL DisplayPlayer        ; Display space at the previous player position
+
+    ; Check if the obstacle reaches the leftmost column
+    LDR R1, [obstaclePosition]
+    CMP R1, #0
+    BEQ resetObstacle
+
+    B gameLoop
+
+resetObstacle
+    ; Reset obstacle to the rightmost column
+    MOV R0, #1              ; Set default row to 1 (top right)
+    BL RandomPosition       ; Get random position (0, 1, or 2)
+    STR R0, [obstaclePosition]
+
+    B gameLoop
+
+    ; Remain here after completion
+stay    B stay
+    endp
+
+; Check for collision between obstacle and player
+CheckCollision function
+    LDR R0, [obstaclePosition]
+    LDR R1, [playerPosition]
+    CMP R0, R1
+    BEQ endGame
+    BX LR
+    endp
+
+endGame
+    ; Your code to end the game (e.g., display a message and halt the program)
+    ; ...
+
+; Display player ('P') at the current position
+DisplayPlayer function
+    BL LCDSetCursorPosition ; Set cursor position based on row (R0) and column (R1)
+    BL LCDData              ; Send character in R3 to LCD
+    BX LR
+    endp
+
+; Move player based on row (R0) and column (R1)
+MovePlayer function
+    LDR R1, [playerPosition]
+    ADD R1, R1, #1          ; Move right by 1 column
+    STR R1, [playerPosition]
+    BX LR
+    endp
+
+; Initialize LCD
+LCDInit function
+    LDR R0, =0x40004C20      ; P3: control pins
+    LDR R1, =0x40004C21      ; P4: data or commands
+    MOV R2, #0xE0            ; 1110 0000
+    STRB R2, [R0, #0x04]     ; outputs pins for EN, RW, RS
+    MOV R2, #0xFF
+    STRB R2, [R1, #0x04]     ; All of Port 4 as output pins to LCD
+    PUSH {LR}
+    MOV R2, #0x38            ; 2 lines, 7x5 characters, 8-bit mode
+    BL LCDCommand            ; Send command in R2 to LCD
+
+    ; ADD INSTRUCTIONS TO TURN ON THE DISPLAY AND THE CURSOR,
+    MOV R2, #0X0E            ; sending hex code
+    BL LCDCommand
+
+    ; CLEAR DISPLAY AND MOVE CURSOR RIGHT
+    MOV R2, #0X01
+    BL LCDCommand
+    MOV R2, #0X06
+    BL LCDCommand
+    POP {LR}
+    BX LR
+    endp
+
+; Send a command to LCD
+LCDCommand function
+    STRB R2, [R1, #0x02]
+    MOV R2, #0x00            ; RS = 0, command register selected, RW = 0, write to LCD
+    ORR R2, EN
+    STRB R2, [R0, #0x02]     ; EN = 1
+    PUSH {LR}
+    BL delay
+
+    MOV R2, #0x00
+    STRB R2, [R0, #0x02]     ; EN = 0 and RS = RW = 0
+    POP {LR}
+    BX LR
+    endp
+
+; Send data to LCD
+LCDData function
+    STRB R3, [R1, #0X02]
+    MOV R2, #0xA0
+    STRB R2, [R0, #0x02]     ; EN = 1
+    PUSH {LR}
+    BL delay
+
+    MOV R3, #0x20
+    STRB R3, [R0, #0x02]
+    POP {LR}
+    BX LR
+    endp
+
+; Add a delay
+delay
