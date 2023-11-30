e92a37d148846bd235918caeae5d676cb32526d8
koltonmyers01 <151096652+koltonmyers01@users.noreply.github.com>
2023-11-30 10:50:27 -0600

    Update4 Proj349.s

--- Proj349/Proj349.s
+++ Proj349/Proj349.s
@@ -6,11 +6,10 @@ RW			equ 0x40	; RW connects to P3.6
 EN			equ 0x80	; EN connects to P3.7
 LO			equ 0x8F
 RO			equ 0xCF
-obstacleFlag DCD OBSTACLE_NONE
-BUTTON_FLAG_ADDR    	EQU 0x20004000   ; Address where the button press flag is stored
-PLAYER_POSITION     	DCD 0
-OBSTACLE_POSITION	DCD 0
-COUNTER 		DCD 0
+BUTTON_FLAG_ADDR    	EQU 	0x20004000   ; Address where the button press flag is stored
+PLAYER_POSITION     	DCD 	0
+OBSTACLE_POSITION		DCD 	0
+COUNTER 				DCD 	0
 			export __main
 
 __main		proc	
@@ -24,7 +23,7 @@ __main		proc
 gameloop
 		; Check for user input and handle player movement
     		LDR R0, =BUTTON_FLAG_ADDR
-   		LDR R1, [R0]
+			LDR R1, [R0]
     		CMP R1, #1
     		BEQ handleButtonPress
 
@@ -40,76 +39,75 @@ gameloop
     		; Update display and delay
     		BL updateDisplay
     		
-		BL delay
+			BL delay
 
-    		B gameLoop
-buttonpress
+    		B gameloop
+handleButtonPress
 		; Toggle player position between 0x80 and 0xC0
-    		LDR R0, [PLAYER_POSITION]   ; Load the current player position
+    		MOV R12, R0  ; Load the current player position
     		CMP R0, #0xC0               ; Compare with 0xC0
     		BEQ setPlayerPosition0x80   ; If equal, set player position to 0x80
     		MOV R1, #0xC0               ; If not equal, set R1 to 0xC0
 		
 setPlayerPosition0x80
-		MOV R1, #0x80               ; Set R1 to 0x80
+			MOV R1, #0x80               ; Set R1 to 0x80
 
 updatePlayerPositionAndDisplay
-    		STR R1, [PLAYER_POSITION]   ; Update the player position
+    		LDR R1, =PLAYER_POSITION   ; Update the player position
 spawnObstacle
     		; Check if an obstacle is already present
-    		CMP R0, #OBSTACLE_NONE
+    		CMP R0, R10
     		BNE obstacleAlreadyPresent  ; Branch if an obstacle is already present
 
       		; Generate a random number (0 or 1) to choose the obstacle position
     		MOV R0, #2          ; Number of positions (0 and 1)
     		BL Rand             ; Call a subroutine to get a random number (0 or 1)
     		CMP R0, #0
-   		BEQ setObstacleTopRight   ; If 0, set obstacle position to top right
+			BEQ setObstacleTopRight   ; If 0, set obstacle position to top right
     		B setObstacleBottomRight  ; If 1, set obstacle position to bottom right
 
 setObstacleTopRight
     		; Set obstacle position to top right (e.g., 0xC0)
-    		MOV R1, L0
-    		STR R1, [OBSTACLE_POSITION]
+    		
+    		MOV R11, #0xCF
     		BX LR
 
 setObstacleBottomRight
     		; Set obstacle position to bottom right (e.g., 0x80)
-    		MOV R1, R0
-    		STR R1, [OBSTACLE_POSITION]
+    		MOV R11, #0x8F
     		BX LR
 obstacleSpawned
     		; Set the obstacle flag to indicate that an obstacle is present
-    		LDR R1, =OBSTACLE_TOP_RIGHT
-    		STR R1, [obstacleFlag]
+    		LDR R1, =OBSTACLE_POSITION
+    		MOV R1, R10
       		BX LR
 Rand
     		; Load the current counter value
-    		LDR R0, [COUNTER]
+    		LDR R0, =COUNTER
 
     		; Increment the counter
     		ADD R0, R0, #1
-    		STR R0, [COUNTER]
+    		MOV R0, R10
 
     		; Generate either 1 or 0 by taking modulo 2
-    		AND R0, R0, #1
+    		AND R0, R10, #1
 
     		; The result is in R0 (either 1 or 0)
 
     		BX LR
 moveObstacle
 		; Load the current obstacle position
-    		LDR R0, [OBSTACLE_POSITION]
+    		LDR R0, =OBSTACLE_POSITION
 
    		; Decrement the obstacle position
     		SUB R0, R0, #1
-    		STR R0, [OBSTACLE_POSITION]
+    		MOV R0, R12
 
     		BX LR
 checkCollision
     		; Load player and obstacle positions
-    		LDR R0, [playerPosition]
-    		LDR R1, [obstaclePosition]
+    		MOV R0, R6
+    		MOV R1, R7
 
     		; Compare player and obstacle positions
     		CMP R0, R1
@@ -121,11 +119,11 @@ checkCollision
 updateDisplay
 
 		; Display player position
-    		LDR R0, [playerPosition]
+    		LDR R0, =PLAYER_POSITION
     		BL LCDData
 
     		; Display obstacle position
-    		LDR R0, [obstaclePosition]
+    		LDR R0, =OBSTACLE_POSITION
     		BL LCDData
 
    		BX LR
@@ -133,7 +131,7 @@ updateDisplay
 stay		B stay					; Remain here after completion
 			endp
 				
-buttonpress				
+obstacleAlreadyPresent			
 LCDInit		function
 					
 			LDR R0, =0x40004C20		; P3: control pins
@@ -167,7 +165,7 @@ LCDInit		function
 LCDCommand	function				; R2 brings in the command byte
 			STRB R2, [R1, #0x02]
 			MOV R2, #0x00			; RS = 0, command register selected, RW = 0, write to LCD
-			ORR R2, EN
+			ORR R2, #EN
 			STRB R2, [R0, #0x02]	; EN = 1
 			PUSH {LR}
 			BL delay
@@ -205,4 +203,4 @@ loop2		SUBS R4, #1
 			BX LR
 			endp
 			
-			end
+				end
